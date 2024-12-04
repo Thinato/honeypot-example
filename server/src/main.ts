82ba0ad56268033209c270ce47babb72c26cc0b6
Paulo Lisecki <piokinovaes@gmail.com>
2024-12-04 08:01:51 -0300

    feat: add frontend

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Replace with your frontend's URL
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+    // credentials: true, // Allow cookies if needed
+  });
+
   await app.listen(process.env.PORT || 3000);
   console.log(process.env.PORT);
 }
